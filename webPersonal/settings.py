eb05cd23a84e85eff7842dc21571da2e3eb06a8c
Jose Maria <chchema26@gmail.com>
2022-08-24 00:32:01 -0500

    Added Portfolio App and creating Models

--- webPersonal/settings.py
+++ webPersonal/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'webPersonal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webPersonal',
+        'USER': 'chema',
+        'PASSWORD': 'St@rdustR1v3n',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -103,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
